288106fd13bdbefbed2fc4c0bc5e605c94344502
Alesr00 <alesr.00@gmail.com>
2021-10-26 00:20:07 -0300

    Initial commit

---
+++ src/main/java/com/migame/Juego/repositories/RepositorioCategoria.java
@@ -0,0 +1,10 @@
+package com.migame.Juego.repositories;
+
+
+import com.migame.Juego.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioCategoria extends JpaRepository<Categoria, Long> {
+}
